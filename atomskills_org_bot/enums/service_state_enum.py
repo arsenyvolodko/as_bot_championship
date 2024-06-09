27c8d4ded3573367c2d0b77ae338ef407bacd0c9
arsenyvolodko <arvolodko@gmail.com>
2024-06-09 18:11:50 +0300

    fix

--- atomskills_org_bot/enums/service_state_enum.py
+++ atomskills_org_bot/enums/service_state_enum.py
@@ -15,4 +15,3 @@ class ServiceStateEnum(Enum):
     SERVICE_STATE_7 = State(ServiceNameEnum.SERVICE_NAME_7.name)
     SERVICE_STATE_8 = State(ServiceNameEnum.SERVICE_NAME_8.name)
     SERVICE_STATE_9 = State(ServiceNameEnum.SERVICE_NAME_9.name)
-    SERVICE_STATE_10 = State(ServiceNameEnum.SERVICE_NAME_10.name)
