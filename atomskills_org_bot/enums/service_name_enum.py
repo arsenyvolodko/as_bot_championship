466661e7a3cf5616b91fb27ebb1f138b162701f2
arsenyvolodko <arvolodko@gmail.com>
2024-06-09 17:39:29 +0300

    fix

--- atomskills_org_bot/enums/service_name_enum.py
+++ atomskills_org_bot/enums/service_name_enum.py
@@ -5,12 +5,12 @@ from atomskills_org_bot import chats_config
 
 class ServiceNameEnum(Enum):
     SERVICE_NAME_1 = chats_config.SERVICE_NAME_1
-    SERVICE_NAME_2 = chats_config.SERVICE_NAME_1
-    SERVICE_NAME_3 = chats_config.SERVICE_NAME_1
-    SERVICE_NAME_4 = chats_config.SERVICE_NAME_1
-    SERVICE_NAME_5 = chats_config.SERVICE_NAME_1
-    SERVICE_NAME_6 = chats_config.SERVICE_NAME_1
-    SERVICE_NAME_7 = chats_config.SERVICE_NAME_1
-    SERVICE_NAME_8 = chats_config.SERVICE_NAME_1
-    SERVICE_NAME_9 = chats_config.SERVICE_NAME_1
-    SERVICE_NAME_10 = chats_config.SERVICE_NAME_1
+    SERVICE_NAME_2 = chats_config.SERVICE_NAME_2
+    SERVICE_NAME_3 = chats_config.SERVICE_NAME_3
+    SERVICE_NAME_4 = chats_config.SERVICE_NAME_4
+    SERVICE_NAME_5 = chats_config.SERVICE_NAME_5
+    SERVICE_NAME_6 = chats_config.SERVICE_NAME_6
+    SERVICE_NAME_7 = chats_config.SERVICE_NAME_7
+    SERVICE_NAME_8 = chats_config.SERVICE_NAME_8
+    SERVICE_NAME_9 = chats_config.SERVICE_NAME_9
+    SERVICE_NAME_10 = chats_config.SERVICE_NAME_10
